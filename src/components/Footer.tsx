a2f2912f60219abb173d1a9749251543e528ecfa
gantug <gantug@protonmail.com>
2025-08-24 23:21:42 +0800

    footer date changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-    <footer className='px-[5.5%] py-[24px] flex flex-row justify-between items-center w-full text-white bg-black font-light'>
-      <div className='text-md md:text-xl  p-3 rounded-xl border-wid border-black'>
-        © 2024 Ivan Hopkins, all rights reserved.
+    <footer className="px-[5.5%] py-[24px] flex flex-row justify-between items-center w-full text-white bg-black font-light">
+      <div className="text-md md:text-xl  p-3 rounded-xl border-wid border-black">
+        © 2025 Ivan Hopkins, all rights reserved.
       </div>
-      <div className='hidden md:flex flex-col md:flex-row gap-5 md:gap-10  p-3'>
-        <a href='mailto:attendtowin@proton.me'>
+      <div className="hidden md:flex flex-col md:flex-row gap-5 md:gap-10  p-3">
+        <a href="mailto:attendtowin@proton.me">
           <div>Email</div>
         </a>
       </div>
