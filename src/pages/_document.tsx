1d216319fac016934fd99a3ce84a10c5c009c7da
gantug <gantug@protonmail.com>
2025-09-19 22:21:53 +0800

    speed insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <SpeedInsights />
       <body>
         <Main />
         <NextScript />
